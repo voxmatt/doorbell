30e81e64a96cee780641bb17f3f4056bd2fa8b7d
voxmatt <matt@getclockwise.com>
2019-08-17 15:48:41 -0700

    forgot to build before publishing

--- dist/doorbell-interface.js
+++ dist/doorbell-interface.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const python_shell_1 = require("python-shell");
 exports.InitDoorbellInterface = () => {
-    return new python_shell_1.PythonShell('~/doorbell/door-monitor.py', {
+    return new python_shell_1.PythonShell('door-monitor.py', {
         mode: 'text',
         pythonPath: '/usr/bin/python3',
         pythonOptions: ['-u'],
-        scriptPath: 'python/',
+        scriptPath: '../',
     });
 };
