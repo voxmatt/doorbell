05f6b029cfaa590526e20be4dc103c8cf8441457
voxmatt <matt@getclockwise.com>
2019-08-17 15:57:07 -0700

    hardcoding the path to the node modules... evil

--- dist/doorbell-interface.js
+++ dist/doorbell-interface.js
@@ -1,16 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const python_shell_1 = require("python-shell");
-const path_1 = __importDefault(require("path"));
 exports.InitDoorbellInterface = () => {
-    const scriptPath = path_1.default.dirname(__dirname).split(path_1.default.sep).pop();
+    // const scriptPath = path.dirname(__dirname).split(path.sep).pop();
     return new python_shell_1.PythonShell('door-monitor.py', {
         mode: 'text',
         pythonPath: '/usr/bin/python3',
         pythonOptions: ['-u'],
-        scriptPath,
+        scriptPath: '/usr/lib/node_modules/homebridge-gate-latch/',
     });
 };
--- src/doorbell-interface.ts
+++ src/doorbell-interface.ts
@@ -2,11 +2,11 @@ import { PythonShell } from 'python-shell'
 import path from 'path';
 
 export const InitDoorbellInterface = () => {
-  const scriptPath = path.dirname(__dirname).split(path.sep).pop();
+  // const scriptPath = path.dirname(__dirname).split(path.sep).pop();
   return new PythonShell('door-monitor.py', {
     mode: 'text',
     pythonPath: '/usr/bin/python3',
     pythonOptions: ['-u'],
-    scriptPath,  
+    scriptPath: '/usr/lib/node_modules/homebridge-gate-latch/',  
   });
 }
